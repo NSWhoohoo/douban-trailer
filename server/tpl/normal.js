2089aaaca7db4e62795b202877c691fbf9a6b485
NSWhoohoo <nswhoohoo@gmail.com>
2019-02-25 22:15:20 +0800

    feat: 新增HTML静态模版文件

---
+++ server/tpl/normal.js
@@ -0,0 +1,25 @@
+module.exports = `
+    <DOCTYPE html>
+    <html>
+        <head>
+            <meta charset='utf-8'></meta>
+            <meta name='viewport' content='width=device-width initial-scale=1'></meta>
+            <title>Koa Server HTML</title>
+            <script src="https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js"></script>
+            <link href="https://cdn.bootcss.com/twitter-bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+            <script src="https://cdn.bootcss.com/twitter-bootstrap/4.2.1/js/bootstrap.bundle.min.js"></script>
+        </head>
+        <body>
+            <div class='container'>
+                <div class='row'>
+                    <div class='col-md-8'>
+                        <h1>Hi HW</h1>
+                        <p>This is HW</p>
+                    </div>
+                    <div class='col-md-4'>
+                        <p>测试静态HTML页面</p>
+                    </div>
+                </div>
+            </div>
+        </body>
+    </html>
